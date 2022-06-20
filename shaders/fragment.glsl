7025375adabba0d14853bb9f62f94c5ce7a38b61
Frode Oijord <foijord@gmail.com>
2022-06-20 15:48:23 +0200

    shader to draw mip levels

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -7,6 +7,16 @@ layout(location = 0) out vec4 FragColor0;
 layout(location = 1) out vec4 FragColor1;
 
 void main() {
-	FragColor0 = texture(Image, TexCoord);
-	FragColor1 = texture(Image, TexCoord);
+	// FragColor1 = texelFetch(Image, (ivec2(gl_FragCoord.xy) / 4) * 4, 0);
+	// FragColor1 = texelFetch(Image, ivec2(gl_FragCoord.xy) / 4, 2);
+
+	ivec2 offset = ivec2(0);
+
+	for (int i = 0; i < 5; i++) {
+		if (gl_FragCoord.y > offset.y) {
+			FragColor1 = texelFetch(Image, ivec2(gl_FragCoord.xy - ivec2(0, offset.y)) * int(pow(2, i)), 0);
+			FragColor1 = texelFetch(Image, ivec2(gl_FragCoord.xy - ivec2(0, offset.y)), i);
+		}
+		offset.y += textureSize(Image, i).y;
+	}
 }

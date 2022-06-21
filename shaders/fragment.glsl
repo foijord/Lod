7d811ba69e648b4299622932e127ff33d367e8cb
Frode Oijord <foijord@gmail.com>
2022-06-21 12:43:29 +0200

    updated Innovator

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -7,12 +7,13 @@ layout(location = 0) out vec4 FragColor0;
 layout(location = 1) out vec4 FragColor1;
 
 void main() {
-	// FragColor1 = texelFetch(Image, (ivec2(gl_FragCoord.xy) / 4) * 4, 0);
-	// FragColor1 = texelFetch(Image, ivec2(gl_FragCoord.xy) / 4, 2);
+	// FragColor1 = texelFetch(Image, ivec2(gl_FragCoord.xy), 0);
+	// FragColor1 = texelFetch(Image, ivec2(gl_FragCoord.xy), 2);
+	// FragColor1 = texelFetch(Image, (ivec2(gl_FragCoord.xy)) * 4, 0);
 
 	ivec2 offset = ivec2(0);
 
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 7; i++) {
 		if (gl_FragCoord.y > offset.y) {
 			FragColor1 = texelFetch(Image, ivec2(gl_FragCoord.xy - ivec2(0, offset.y)) * int(pow(2, i)), 0);
 			FragColor1 = texelFetch(Image, ivec2(gl_FragCoord.xy - ivec2(0, offset.y)), i);
